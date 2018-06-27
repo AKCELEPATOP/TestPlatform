23326f89d0e0b51a2bebf5f5d8f98e383e186dcf
AKCELEPATOP <sasha98zem@gmail.com>
2018-06-27 21:49:12 +0400

    starting categories

---
+++ Wpf/TestService/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestService.ViewModels
+{
+    [DataContract]
+    public class CategoryViewModel
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public List<QuestionViewModel> Questions { get; set; }
+    }
+}
---
+++ Wpf/TestService/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestService.ViewModels
+{
+    [DataContract]
+    public class QuestionViewModel
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Text { get; set; }
+
+        [DataMember]
+        public string Complexity { get; set; }
+
+        [DataMember]
+        public bool Active { get; set; }
+
+        [DataMember]
+        public List<AnswerViewModel> Answers { get; set; }
+    }
+}
