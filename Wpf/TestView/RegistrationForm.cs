e5e407a84a11e82ea4c162069e0e7c68a9d2b062
Tumanov-Nikita <grooov173@yandex.ru>
2018-06-30 00:10:32 +0400

    Fix Register form TRY №2

--- Wpf/TestView/RegistrationForm.cs
+++ Wpf/TestView/RegistrationForm.cs
@@ -15,8 +15,8 @@ namespace TestView
         {
             try
             {
-                if (textBoxFIO.TextLength == 0 || textBoxLogin.TextLength < 8 ||
-                    textBoxPassword1.TextLength == 0 || textBoxPassword2.TextLength == 0)
+                if (textBoxFIO.TextLength != 0 || textBoxLogin.TextLength > 8 ||
+                    textBoxPassword1.TextLength != 0 || textBoxPassword2.TextLength != 0)
                 {
                     if (textBoxPassword1.Text == textBoxPassword2.Text)
                     {
