a633f8ce8b4bf1d8a33a5a93e2303f6c21ff7df1
AKCELEPATOP <sasha98zem@gmail.com>
2018-06-30 12:16:39 +0400

    fix form

--- Wpf/TestView/MainForm.Designer.cs
+++ Wpf/TestView/MainForm.Designer.cs
@@ -18,6 +18,7 @@
                 components.Dispose();
             }
             base.Dispose(disposing);
+            ApiClient.Logout();
             if (parent != null)
                 parent.DisposeForm();
         }
