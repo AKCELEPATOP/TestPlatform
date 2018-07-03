27b44f0b5831fcf34cca8f67b345ae1abb19ddac
gdeon1337 <gdeon1337@gmail.com>
2018-07-03 21:17:38 +0400

    тутуру

---
+++ Wpf/TestView/RegistrationForm.designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TestView
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.registrate = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPassword2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxFIO = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 130);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 162);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль:";
+            // 
+            // registrate
+            // 
+            this.registrate.Location = new System.Drawing.Point(107, 224);
+            this.registrate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.registrate.Name = "registrate";
+            this.registrate.Size = new System.Drawing.Size(153, 28);
+            this.registrate.TabIndex = 3;
+            this.registrate.Text = "Зарегистрировать";
+            this.registrate.UseVisualStyleBackColor = true;
+            this.registrate.Click += new System.EventHandler(this.registrate_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(148, 128);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxLogin.MaxLength = 16;
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(221, 22);
+            this.textBoxLogin.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.textBoxLogin, "8 символов и как минимум один буквенный и без спец символов");
+            this.textBoxLogin.MaxLength = 32;
+            // 
+            // textBoxPassword1
+            // 
+            this.textBoxPassword1.Location = new System.Drawing.Point(148, 160);
+            this.textBoxPassword1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxPassword1.MaxLength = 16;
+            this.textBoxPassword1.Name = "textBoxPassword1";
+            this.textBoxPassword1.Size = new System.Drawing.Size(221, 22);
+            this.textBoxPassword1.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.textBoxPassword1, "как минимум 5 символов, хотя бы 1 буква, не принимается кирилица");
+            this.textBoxPassword1.UseSystemPasswordChar = true;
+            this.textBoxPassword1.TextChanged += new System.EventHandler(this.textBoxPassword1_TextChanged);
+            this.textBoxPassword1.MaxLength = 32;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(19, 25);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(339, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Введите данные нового пользователя";
+            // 
+            // textBoxPassword2
+            // 
+            this.textBoxPassword2.Location = new System.Drawing.Point(148, 194);
+            this.textBoxPassword2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxPassword2.MaxLength = 16;
+            this.textBoxPassword2.Name = "textBoxPassword2";
+            this.textBoxPassword2.Size = new System.Drawing.Size(221, 22);
+            this.textBoxPassword2.TabIndex = 7;
+            this.textBoxPassword2.UseSystemPasswordChar = true;
+            this.textBoxPassword2.TextChanged += new System.EventHandler(this.textBoxPassword2_TextChanged);
+            this.textBoxPassword2.MaxLength = 32;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 194);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Повторите пароль:";
+            // 
+            // textBoxFIO
+            // 
+            this.textBoxFIO.Location = new System.Drawing.Point(148, 68);
+            this.textBoxFIO.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxFIO.MaxLength = 16;
+            this.textBoxFIO.Name = "textBoxFIO";
+            this.textBoxFIO.Size = new System.Drawing.Size(221, 22);
+            this.textBoxFIO.TabIndex = 9;
+            this.toolTip1.SetToolTip(this.textBoxFIO, "Должны быть имя, фамилия, и отчество только буквенными символами");
+            this.textBoxFIO.MaxLength = 32;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 70);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ФИО:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 101);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Введите email:";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(148, 97);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxEmail.MaxLength = 32;
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(221, 22);
+            this.textBoxEmail.TabIndex = 13;
+            this.textBoxEmail.MaxLength = 32;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 100;
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 100;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.OwnerDraw = true;
+            this.toolTip1.ReshowDelay = 20;
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.StripAmpersands = true;
+            this.toolTip1.ToolTipTitle = "Подсказка";
+            // 
+            // RegistrationForm
+            // 
+            this.AcceptButton = this.registrate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 268);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxFIO);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPassword2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPassword1);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.registrate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "RegistrationForm";
+            this.Padding = new System.Windows.Forms.Padding(20, 74, 20, 20);
+            this.Text = "Регистрация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button registrate;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPassword2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxFIO;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
