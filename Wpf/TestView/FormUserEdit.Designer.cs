4777626ea2ebda66f5ed39a1a1adf9359c70f636
gdeon1337 <gdeon1337@gmail.com>
2018-07-04 14:10:00 +0400

    фичи для графика

--- Wpf/TestView/FormUserEdit.Designer.cs
+++ Wpf/TestView/FormUserEdit.Designer.cs
@@ -46,43 +46,48 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 37);
+            this.label1.Location = new System.Drawing.Point(23, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.Size = new System.Drawing.Size(42, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "ФИО";
             // 
             // textBoxFIO
             // 
-            this.textBoxFIO.Location = new System.Drawing.Point(57, 34);
-            this.textBoxFIO.Name = "textBoxFIO";
-            this.textBoxFIO.Size = new System.Drawing.Size(177, 20);
-            this.textBoxFIO.TabIndex = 1;
+            this.textBoxFIO.Location = new System.Drawing.Point(76, 42);
+            this.textBoxFIO.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxFIO.MaxLength = 32;
+            this.textBoxFIO.Name = "textBoxFIO";
+            this.textBoxFIO.Size = new System.Drawing.Size(235, 22);
+            this.textBoxFIO.TabIndex = 1;
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Location = new System.Drawing.Point(81, 69);
-            this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(153, 20);
-            this.textBoxEmail.TabIndex = 9;
+            this.textBoxEmail.Location = new System.Drawing.Point(108, 85);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxEmail.MaxLength = 32;
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(203, 22);
+            this.textBoxEmail.TabIndex = 9;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 72);
+            this.label4.Location = new System.Drawing.Point(23, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.Size = new System.Drawing.Size(42, 17);
             this.label4.TabIndex = 8;
             this.label4.Text = "Email";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 100);
+            this.label5.Location = new System.Drawing.Point(23, 123);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.Size = new System.Drawing.Size(55, 17);
             this.label5.TabIndex = 10;
             this.label5.Text = "Группа";
             // 
@@ -90,51 +95,56 @@
             // 
             this.comboBoxGroups.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxGroups.FormattingEnabled = true;
-            this.comboBoxGroups.Location = new System.Drawing.Point(81, 100);
-            this.comboBoxGroups.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxGroups.Location = new System.Drawing.Point(108, 123);
+            this.comboBoxGroups.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxGroups.Name = "comboBoxGroups";
-            this.comboBoxGroups.Size = new System.Drawing.Size(153, 21);
+            this.comboBoxGroups.Size = new System.Drawing.Size(203, 24);
             this.comboBoxGroups.TabIndex = 11;
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(81, 168);
-            this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(153, 20);
-            this.textBoxPassword.TabIndex = 15;
+            this.textBoxPassword.Location = new System.Drawing.Point(108, 207);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxPassword.MaxLength = 16;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(203, 22);
+            this.textBoxPassword.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 171);
+            this.label3.Location = new System.Drawing.Point(23, 210);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.Size = new System.Drawing.Size(57, 17);
             this.label3.TabIndex = 14;
             this.label3.Text = "Пароль";
             // 
             // textBoxUserName
             // 
-            this.textBoxUserName.Location = new System.Drawing.Point(81, 135);
-            this.textBoxUserName.Name = "textBoxUserName";
-            this.textBoxUserName.Size = new System.Drawing.Size(153, 20);
-            this.textBoxUserName.TabIndex = 13;
+            this.textBoxUserName.Location = new System.Drawing.Point(108, 166);
+            this.textBoxUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxUserName.MaxLength = 16;
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(203, 22);
+            this.textBoxUserName.TabIndex = 13;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 138);
+            this.label2.Location = new System.Drawing.Point(23, 170);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(73, 17);
             this.label2.TabIndex = 12;
             this.label2.Text = "Username";
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(15, 204);
+            this.buttonSave.Location = new System.Drawing.Point(20, 251);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.Size = new System.Drawing.Size(100, 28);
             this.buttonSave.TabIndex = 6;
             this.buttonSave.Text = "Сохранить";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -142,9 +152,10 @@
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(158, 204);
+            this.buttonCancel.Location = new System.Drawing.Point(211, 251);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(100, 28);
             this.buttonCancel.TabIndex = 7;
             this.buttonCancel.Text = "Отмена";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -153,18 +164,19 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(38, 9);
+            this.label6.Location = new System.Drawing.Point(51, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(182, 13);
+            this.label6.Size = new System.Drawing.Size(234, 17);
             this.label6.TabIndex = 16;
             this.label6.Text = "Добавление нового пользователя";
             // 
             // FormUserEdit
             // 
             this.AcceptButton = this.buttonSave;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 236);
+            this.ClientSize = new System.Drawing.Size(335, 290);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBoxPassword);
             this.Controls.Add(this.label3);
@@ -178,8 +190,9 @@
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.textBoxFIO);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormUserEdit";
-            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
+            this.Padding = new System.Windows.Forms.Padding(20, 74, 20, 20);
             this.Text = "Пользователь";
             this.Load += new System.EventHandler(this.FormUserEdit_Load);
             this.ResumeLayout(false);
